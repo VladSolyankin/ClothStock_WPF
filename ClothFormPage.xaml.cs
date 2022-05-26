8a43726ed821a189509707e76147c71d259bbc44
Влад Солянкин <vlad.solyankin97@mail.ru>
2022-05-26 23:20:35 +0300

    new product or existing

---
+++ ClothFormPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ClothStock_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClothStock_WPF
+{
+    /// <summary>
+    /// Interaction logic for ClothFormPage.xaml
+    /// </summary>
+    public partial class ClothFormPage : Page
+    {
+        Cloth currentCloth;
+        bool newProduct;
+        public ClothFormPage(Cloth cloth)
+        {
+            InitializeComponent();
+            if (cloth == null)
+            {
+                currentCloth = new Cloth();
+                newProduct = true;
+            }
+            else
+            {
+                currentCloth = cloth;
+                newProduct = false;
+            }
+            DataContext = currentCloth;
+        }
+    }
+}
