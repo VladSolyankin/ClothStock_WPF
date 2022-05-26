3cebebd4993575e9beeb8b9efbe466d44f88df4d
Влад Солянкин <vlad.solyankin97@mail.ru>
2022-05-26 23:21:37 +0300

    navigation and functions in Cloth List

--- ClothListPage.xaml.cs
+++ ClothListPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClothStock_ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,36 @@ namespace ClothStock_WPF
         public ClothListPage()
         {
             InitializeComponent();
+        }
+
+        private void DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            List<Cloth> ProductsForDel = gridStock.SelectedItems.Cast<Cloth>().ToList();
+            if (ProductsForDel.Count == 0)
+            {
+                MessageBox.Show("Выделите продукты для удаления");
+                return;
+            }
+            if (MessageBox.Show($"Вы действительно хотите удалить {ProductsForDel.Count} товаров?", "Отменить действие будет нельзя", MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                foreach (var prod in ProductsForDel)
+                {
+                    ManagerModel.Stock.Remove(prod);
+                }
+                gridStock.Items.Refresh();
+            }
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            ManagerNavigation.MainFrame.Navigate(new ClothFormPage(null));
+        }
+
+        private void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Cloth selectProduct = (sender as Button).DataContext as Cloth;
+            ManagerNavigation.MainFrame.Navigate(new ClothFormPage(selectProduct));
         }
     }
 }
