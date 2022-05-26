a58311948d1ae2db1e73e666d20b7d063958e850
Влад Солянкин <vlad.solyankin97@mail.ru>
2022-05-26 23:18:25 +0300

    manager_model
    add model for stock

---
+++ ManagerModel.cs
@@ -0,0 +1,21 @@
+﻿using ClothStock_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothStock_WPF
+{
+    public class ManagerModel
+    {
+        public static Stock Stock { get; set; }
+        public ManagerModel()
+        {
+            Stock = new Stock();
+            Stock.Add(new Cloth());
+            Stock.Add(new Cloth("Индийский шёлк", ProducingFactory.БТК_Текстиль,
+                Types.Натуральная, 200, new DateTime(2003, 12, 18), 10, Markup.десять));
+        }
+    }
+}
